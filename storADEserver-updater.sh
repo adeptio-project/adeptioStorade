69478e9977be8f8551cc8cebc197459c307f4654
roothere <38818888+adeptio-project@users.noreply.github.com>
2018-12-17 17:57:28 +0200

    service rand sleep temp topology

--- storADEserver-updater.sh
+++ storADEserver-updater.sh
@@ -11,8 +11,16 @@ report_as_storADE_node=$(curl -s https://storade.adeptio.cc/register_storade_nod
 cd $Work_dir
 new_version_available=$($git fetch origin && $git status | grep -c "git pull")
 
+service_stop_sleep=5
+random_sleep=$(echo $(($RANDOM % 100)))
+
 if [ "$new_version_available" -eq 0 ]; then
    echo "$date storADEserver version up-to-date"
 else
-   echo "$date storADEserver new version available" && sudo systemctl stop storADEserver && sleep 30 && $git pull && sudo systemctl start storADEserver
+   echo "$date storADEserver new version available";
+   sleep $random_sleep; 
+   sudo systemctl stop storADEserver; 
+   sleep $service_stop_sleep; 
+   $git pull;
+   sudo systemctl start storADEserver
 fi
